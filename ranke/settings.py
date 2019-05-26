a6f24daae015b46a8774b730cd602eb6cebe83a2
Rotaka92 <robin.tappert@gmx.de>
2019-05-26 18:15:28 +0200

    'static1'

--- ranke/settings.py
+++ ranke/settings.py
@@ -116,15 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
@@ -156,3 +148,20 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+
+	
+	
+	
+	
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = 'C:/Users/Robin/Desktop/ranke/static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'C:/Users/Robin/Desktop/ranke/media'
+
+
+
+
+
